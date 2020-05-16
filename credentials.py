4b9901e742de77e389ef09acece9d32eb225cd8d
Thiago <thiago.giachetto@gmail.com>
2020-05-16 14:54:51 -0300

    create doc with credentials to connect to git using colab

---
+++ credentials.py
@@ -0,0 +1,8 @@
+# replace with your Github username 
+GIT_USERNAME = "GIT_USERNAME"
+
+# definitely replace with your
+GIT_TOKEN = "YOUR_GITHUB_TOKEN" 
+
+# Replace with your github repository in this case we want 
+GIT_REPOSITORY = "ds-for-healthcare-final-project-autism" 
